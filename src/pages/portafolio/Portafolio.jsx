60e96bfaafa39cfabc7ba3c338c495ccef5e9b51
Joshe1601 <jmorillosp@pucp.edu.pe>
2023-12-22 22:34:43 -0500

    Pagina de portafolio realizada

--- src/pages/portafolio/Portafolio.jsx
+++ src/pages/portafolio/Portafolio.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import {portfolio} from "../../data.jsx";
+import PortfolioItem from "../../components/PortfolioItem.jsx";
+import "./portafolio.css";
 
 const Portafolio = () => {
     return (
-        <div>
-        <h1>Portafolio</h1>
-        </div>
+        <section className="portfolio section">
+            <h2 className="section__title">Mis <span>Proyectos</span></h2>
+
+            <div className="portfolio__container container grid">
+                {portfolio.map((item) => {
+                    return <PortfolioItem key={item.id} {...item} />
+                })}
+            </div>
+        </section>
     );
 }
 
