cf5bcb0fdcf424bbb49f31d6f8c66dc633f6e82d
Joshe1601 <jmorillosp@pucp.edu.pe>
2023-12-25 20:35:27 -0500

    Temas y colores aplicados

--- src/components/ThemeItem.jsx
+++ src/components/ThemeItem.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const ThemeItem = () => {
-    return(
-        <div>ThemeItem</div>
-    )
+const ThemeItem = ({color, img, changeColor}) => {
+    return <img src={img} alt="Imagen de tema" className="theme__img" onClick={() =>
+    {changeColor(color)}}/>
+
 }
 
 export default ThemeItem;
