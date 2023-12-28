d4e1627905a12556e6c727c5cb45707c10eda44e
Joshe1601 <jmorillosp@pucp.edu.pe>
2023-12-28 13:46:48 -0500

    A desplegar

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://Joshe1601.github.io/MiPortafolio/'
+  base: 'https://joshe1601.github.io/MiPortafolio/'
 })
